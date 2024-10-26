7e74e39bcba1ee18d240b1e0972c84cd1dd84db7
gabriel-leonel <gabriel.leonel2016@gmail.com>
2024-10-26 13:41:37 -0300

    Refatorado o input de alunos e sistema de menu

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Variaveis
-int alunos[30];
+char alunos[30][20]; // O segundo valor permite que receba string de ate 50 letras
 int id[30];
 float notas1[30];
 float notas2[30];
@@ -9,45 +9,73 @@ float notas3[30];
 
 // Funçao para cadastro de Alunos, ID, e notas 1 2 3
 int cadastroAluno(){
+    int qtdAlunos;
     
-    printf("Digite o nome do aluno: ");
-    scanf("%d", &alunos);
+    printf("Quantos Alunos deseja cadastrar? ");
+    scanf("%d", &qtdAlunos);
 
-    printf("Digite o ID do aluno: ");
-    scanf("%d", &id);
+    for(int i = 0; i < qtdAlunos; i++){
+        printf("Digite o primeiro nome do aluno %d: ",i+1);
+        scanf("%s", alunos[i]);
 
-    printf("Digite a nota 1: ");
-    scanf("%f", &notas1);
+        id[i] = i + 1; // Assemelha o id automaticamente
 
-    printf("Digite a nota 2: ");
-    scanf("%f", &notas2);
+        printf("\nDigite a nota 1: ");
+        scanf("%f", &notas1[i]);
 
-    printf("Digite a nota 3: ");
-    scanf("%f", &notas3);
+        printf("\nDigite a nota 2: ");
+        scanf("%f", &notas2[i]);
 
+        printf("\nDigite a nota 3: ");
+        scanf("%f", &notas3[i]);
+
+        printf("\n");
+    }
+    for(int i=0;i<qtdAlunos;i++){
+        printf("Aluno %d: %s\n", id[i], alunos[i]);
+        printf("Nota 1: %.2f\n", notas1[i]);
+        printf("Nota 2: %.2f\n", notas2[i]);
+        printf("Nota 3: %.2f\n", notas3[i]);
+        printf("\n");
+    }
 }
 
 // Funcao para Menu com laço de repetição
 int menu(){
     int escolha;
 
-    printf("\n\n\n\n\n\n\n");
     printf("========== SISTEMA DE NOTAS ==========");
-    printf("1- Cadastrar novo aluno");
-    printf("2- Media do aluno especifico");
-    printf("3- Relatorio de notas geral");
-    printf("4- Sair");
 
-    while(escolha < 4)
-    {
-        if(escolha == 1){
-            cadastroAluno();
-        }else if(escolha == 2){
-            mediaAluno();
-        }else if(escolha == 3){
-            relatorioNotas();
-        }                    
+    if(id[0] == 0){
+        printf("\nNenhum aluno cadastrado\n");
+        cadastroAluno();
+    }
+
+    printf("\nO que deseja fazer?");
+    printf("\n1- Cadastrar novo aluno");
+    printf("\n2- Media do aluno especifico");
+    printf("\n3- Relatorio de notas geral");
+    printf("\n4- Sair \n");
+
+    scanf("%d", &escolha);
+    if(escolha == 1){
+        cadastroAluno();
+    }
+    else if(escolha == 2){
+        // Funcao Media de Aluno
+    }
+    else if(escolha == 3){
+        // Funcao Relatorio de Notas
+    }
+    else if(escolha == 4){
+        return 0;
+    }
+    else{
+        printf("Opcao invalida, fechando o programa");
     }
 }
 
+int main(){
+    menu();
+}
 // FUNCOES PENDENTES: Media de Aluno e Relatorio de Notas
