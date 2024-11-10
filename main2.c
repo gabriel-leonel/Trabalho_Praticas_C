96b97b8f007f191e2700bc33aec86a91793e3738
Gabriel Leonel Santos da Silva <gabriel.leonel2016@gmail.com>
2024-11-10 18:30:45 -0300

    Update main2.c 10/11/2024

--- main2.c
+++ main2.c
@@ -2,11 +2,17 @@
 
 char nomeAlunos[30][20]; // Armazena o nomes dos alunos de ate 20 caracteres
 float infoAluno[30][4];  // Indice 0 = ID, Indice 1,2,3 = Notas 1,2,3
-int alunosCadastrados=1;
+int alunosCadastrados=0;
 
-int cadastroAluno(int alunosCadastrados2){
+int cadastroAluno(int alunosCadastrados2)
+{
     alunosCadastrados++;
-
+    if (alunosCadastrados >= 30)
+    {
+        printf("Quantidade maxima de alunos cadastrada.");
+    }
+    else
+    {
         // input de informacoes
         for (int i = 0; i < alunosCadastrados2; i++)
         {
@@ -23,31 +29,66 @@ int cadastroAluno(int alunosCadastrados2){
         }
 
         // output das mesmas informacoes
-        for (int i = 0; i < alunosCadastrados2; i++){
+        for (int i = 0; i < alunosCadastrados2; i++)
+        {
             printf("Nome: %s \n", nomeAlunos[i]);
             printf("ID: %.f \n", infoAluno[i][0]);
-            printf("Nota1%.2f\n", infoAluno[i][1]);
-            printf("Nota2%.2f\n", infoAluno[i][2]);
-            printf("Nota3%.2f\n", infoAluno[i][3]);
+            printf("Nota1: %.2f\n", infoAluno[i][1]);
+            printf("Nota2: %.2f\n", infoAluno[i][2]);
+            printf("Nota3: %.2f\n", infoAluno[i][3]);
         }
+    }
 }
 
-void calcularMedia(){
+void calcularMedia()
+{
     int id_digitado = 0;
 
     printf("Digite o ID do aluno:");
     scanf("%d", &id_digitado);
 
-    for(int i = 0; i < alunosCadastrados; i++){
-        
+    for (int i = 0; i < alunosCadastrados; i++)
+    {
     }
     printf("%d", infoAluno[id_digitado][0]);
+}
+
+void menu()
+{
+    int escolha;
+
+    printf("\nO que deseja fazer?");
+    printf("\n1- Cadastrar novo aluno");
+    printf("\n2- Media do aluno especifico");
+    printf("\n3- Relatorio de notas geral");
+    printf("\n4- Sair \n");
+
+    scanf("%d", &escolha);
+    if (escolha == 1)
+    {
+        cadastroAluno(alunosCadastrados);
+    }
+    else if (escolha == 2)
+    {
+        // Funcao Media de Aluno
+        calcularMedia();
+    }
+    else if (escolha == 3)
+    {
+        // Funcao Relatorio de Notas
+    }
+    else if (escolha == 4)
+    {
+    }
+    else
+    {
+        printf("Opcao invalida, fechando o programa");
+    }
 }
 
 int main()
 {
     cadastroAluno(alunosCadastrados);
-
-    calcularMedia();    
+    menu();
+    calcularMedia();
 }
-
